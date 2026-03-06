2f6bed1bb6730cba2c6828f3fa816e2a9ae6bbf1
junboo1234 <168405856+junboo1234@users.noreply.github.com>
2026-03-06 16:32:09 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header className="APP">
+        <h1>반가워요! 나만의 첫 웹사이트입니다.</h1>
+        <p>앞으로 이곳을 멋지게 채워나갈 거예요.</p>
+        <button onClick={() => alert('버튼을 누르셨군요!')}>클릭해보세요</button>
+      </header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
