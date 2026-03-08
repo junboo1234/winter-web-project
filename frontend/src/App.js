c27ce0cb9985ea17467c918de3d564a5f983d24c
junboo1234 <168405856+junboo1234@users.noreply.github.com>
2026-03-09 04:03:11 +0900

    design frontend Navbar css 및 app에 Navbar 추가
    기존 App의 예시를 삭제 후 Navbar에 menus의 값을 가진 버튼 5개 추가하고 Navbar의 css 정의

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './Navbar.css';
+
+function NavItem({label}){
+  return( 
+    <button className="nav-button" >
+      {label}
+    </button>
+  );
+}
 
 function App() {
+  const menus = ["home","menu","mood","my page","about us"];
   return (
-    <div className="App">
-      <header className="APP">
-        <h1>반가워요! 나만의 첫 웹사이트입니다.</h1>
-        <p>앞으로 이곳을 멋지게 채워나갈 거예요.</p>
-        <button onClick={() => alert('버튼을 누르셨군요!')}>클릭해보세요</button>
-      </header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="navbar">
+      {menus.map((menu,index)=>(
+        <NavItem key={index} label={menu}/>
+      ))}
     </div>
+
   );
 }
 
