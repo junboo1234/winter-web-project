92c2941d9b0747c6e711a7da29898aa4ff5c357a
junboo1234 <168405856+junboo1234@users.noreply.github.com>
2026-03-17 23:15:46 +0900

    feat(home.js, home.css) 화살표 누르면 슬라이드 이동 및 화살표에 마우스 올리면 타이머가 멈추는 기능 추가
    화살표를 누르면 슬라이드가 이동하도록 함 화살표 위에 마우스를 올리면 time을 pause해서 슬라이드가 움직이지 않도록 설정함

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { ChevronLeft, ChevronRight } from 'lucide-react'; //npm install lucide-react 
+
 const SLIDE_ITEMS = [
     { id: 1, title: "오늘 하루 기분이 어떠신가요?", sub: "당신의 기분에 맞춘 음악을 추천합니다.", color: "#6c5ce7" },
     { id: 2, title: "지친 당신을 위한 위로", sub: "음악이 당신의 마음을 안아줄 거예요.", color: "#a29bfe" },
@@ -6,21 +8,45 @@ const SLIDE_ITEMS = [
     { id: 4, title: "새로운 음악 탐색1", sub: "지금 당신의 감정에 어울리는 곡을 찾아보세요1.", color: "#000000" }
 ];
 
+
 function Home() { 
+
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    
+    const [isPaused, setIsPaused] = useState(false);
+
+
+
     useEffect(() => {
+        if(isPaused == true){
+            return;
+        }
         const timer = setTimeout(() => {
             setCurrentIndex((prev) => (prev + 1) % SLIDE_ITEMS.length);
-        
-        }, 3000);
+        }, 1000);
         return () => clearTimeout(timer); 
-    }, [currentIndex]);
+    }, [currentIndex, setIsPaused]);
+
+    const nextSlide = () => {
+        setCurrentIndex((prev) => (prev + 1) % SLIDE_ITEMS.length);
+    };
+
+    // 이전 슬라이드로 이동
+    const prevSlide = () => {
+        setCurrentIndex((prev) => (prev - 1 + SLIDE_ITEMS.length) % SLIDE_ITEMS.length);
+    };
 
     return (
         <div className="home-container">
             {/* 4. 긴 기차 부분: currentIndex에 따라 왼쪽으로 밀립니다 */}
+            <button 
+                className="arrow-button left" 
+                onClick={prevSlide} 
+                onMouseEnter={() => setIsPaused(true)} 
+                onMouseLeave={() => setIsPaused(false)}
+            >
+                <ChevronLeft size={32} color="white" strokeWidth={1.5} />
+            </button>
             <div 
                 className="slide-wrapper" 
                 style={{ transform: `translateX(-${currentIndex * 100}%)` }}
@@ -31,10 +57,19 @@ function Home() {
                             <h1>{item.title}</h1>
                             <p>{item.sub}</p>
                             <button className="start-button">지금 기록하기</button>
+                            
                         </section>
                     </div>
                 ))}
             </div>
+            <button 
+                className="arrow-button right" 
+                onClick={nextSlide} 
+                onMouseEnter={() => setIsPaused(true)} 
+                onMouseLeave={() => setIsPaused(false)}
+            >
+                <ChevronRight size={32} color="white" strokeWidth={1.5} />
+            </button>
 
             {/* 5. 아래쪽 점(인디케이터) 표시 */}
             <div className="slide-dots">
